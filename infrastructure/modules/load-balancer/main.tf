ab1eaac78e24c71cadedcbfa68d7aec604e7e64e
herculan0 <lucasgherculano@gmail.com>
2025-03-29 15:33:00 -0300

    Create the A Record

--- infrastructure/modules/load-balancer/main.tf
+++ infrastructure/modules/load-balancer/main.tf
@@ -126,3 +126,15 @@ resource "aws_lb_listener" "https" {
     aws_acm_certificate_validation.cert
   ]
 }
+
+resource "aws_route53_record" "alb" {
+  zone_id = var.route53_zone_id
+  name    = var.domain_name
+  type    = "A"
+  
+  alias {
+    name                   = aws_lb.main.dns_name
+    zone_id                = aws_lb.main.zone_id
+    evaluate_target_health = true
+  }
+}
